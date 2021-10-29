91cd8353dddaf12b1558b67307f9070302aa59d2
megabytesofrem <dhillonwilk@gmail.com>
2021-10-29 17:36:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# villager-bdays
+A simple Vue web app to display the villager whose birthday is today.
+
+## Running
+`npm run dev` starts a development server and opens it in your browser.
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="app">
+        <h1>On {{date}} it is {{villager.gender == 'Male' ? '♂' : '♀'}} {{villager.name['name-USen']}}s birthday!</h1>
+        <img id="image" :src="villager.image_uri">
+
+        <br>
+        <h2>Personality: {{villager.personality}}</h2>
+        <h2>Catchphrase: {{villager['catch-phrase']}}</h2>
+    </div>
+</template>
+
+<script>
+const _ = require('lodash')
+
+import date from 'date-and-time'
+const villagers = require('./assets/villagers.json')
+
+let todayDate
+
+function findBirthday(bday) {
+    for (const key in villagers) {
+        const subObject = villagers[key]
+
+        if (subObject['birthday'] === bday) {
+            return subObject
+        }
+    }
+}
+
+export default {
+    mounted() {
+        
+    },
+    data() {
+        const today = new Date()
+        todayDate = date.format(today, 'D/M')
+
+        return {
+            date: todayDate,
+            villager: findBirthday(todayDate)
+        }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
+
+* {
+    padding: 5px;
+    margin: 0px;
+}
+
+@import './assets/style.css';
+</style>
